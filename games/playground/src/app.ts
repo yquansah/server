0166528004b140baca3d2790ed5639e35bfb0530
Kevin Tang <kevin.tang@twosigma.com>
2021-01-06 22:26:11 -0600

    setup basic character with rotating camera

--- games/playground/src/app.ts
+++ games/playground/src/app.ts
@@ -1,7 +1,7 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, Color3, SceneLoader, CannonJSPlugin } from "@babylonjs/core";
+import { Engine, Scene, Space, Vector3, HemisphericLight, Mesh, Color3, SceneLoader, CannonJSPlugin, MeshBuilder, StandardMaterial, PhysicsImpostor, MotorEnabledJoint, PhysicsJoint, Axis, UniversalCamera } from "@babylonjs/core";
 import * as cannon from "cannon"
 import {setupSky} from "./sky"
 // TODO: figure out how to do eslint with formatting white space
@@ -18,22 +18,56 @@ function setupLighting(config, scene){
 }
 
 function setupMap(scene){
-  const ground = Mesh.CreateGround("ground1", 12, 12, 0, scene);
+  const ground = Mesh.CreateGround("ground", 200, 200, 0, scene);
+  ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.BoxImpostor, { mass:0, restitution: 0.5, friction: 0.5 }, scene);
 }
 
-function setupCharacter(scene){
+function setupCharacter(scene, canvas){
+  const humanoidMaterial = new StandardMaterial("humanoidMaterial", scene);
+  humanoidMaterial.diffuseColor = new Color3(0, 1, 0)
 
-}
+  const humanoidRoot = MeshBuilder.CreateBox("humanoidRoot", {width: 0.5, height: 0.5, depth: 0.5}, scene)
+  humanoidRoot.position = new Vector3(0, 1.75, 0);
+  humanoidRoot.material = humanoidMaterial
+  humanoidRoot.showBoundingBox = true
+  humanoidRoot.physicsImpostor = new PhysicsImpostor(
+    humanoidRoot,
+    PhysicsImpostor.BoxImpostor,
+    {
+        mass: 0,
+        friction: 0.1,
+        restitution: 0.1
+    }, scene);
+  
+  const feet = MeshBuilder.CreateBox("player", {width:1, height:1, depth:1}, scene);
+  feet.position = new Vector3(0, 0.5, 0)
+  feet.material = humanoidMaterial
+  feet.physicsImpostor = new PhysicsImpostor(
+    feet,
+    PhysicsImpostor.BoxImpostor,
+    {
+      mass: 1,
+      friction: 0.1,
+      restitution: 0.1,
+    }, scene);
+  
+  const joint = new MotorEnabledJoint(PhysicsJoint.LockJoint, {
+    mainPivot: new Vector3(0,0,0),
+    connectedPivot: new Vector3(0, 5, 0),
+    mainAxis: Axis.Y,
+    nativeParams: {
+      collision: false
+    }
+  })
 
-function setupCamera(scene, canvas){
-  const camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-  camera.attachControl(canvas, true);
-
+  humanoidRoot.physicsImpostor.addJoint(feet.physicsImpostor, joint)
 
   // TODO NEXT: setup character
   SceneLoader.ImportMesh("", "./assets/", "Xbot.glb", scene, function onSuccess(newMeshes) {
 
   })
+
+  return humanoidRoot
 }
 
 async function main() {
@@ -47,7 +81,7 @@ async function main() {
   // initialize babylon scene and engine
   const engine = new Engine(canvas, true);
   const scene = new Scene(engine);
-  scene.enablePhysics(new Vector3(20,0,0),new CannonJSPlugin(true, 100, cannon))
+  scene.enablePhysics(new Vector3(0, -9.81, 0),new CannonJSPlugin(true, 100, cannon))
 
   // hide/show the Inspector
   window.addEventListener("keydown", (ev) => {
@@ -62,13 +96,17 @@ async function main() {
   });
 
   setupMap(scene)
-  setupCamera(scene, canvas)
   setupLighting(lightingConfig, scene);
   setupSky(scene)
-  setupCharacter(scene)
+  const humanoidRoot = setupCharacter(scene, canvas)
+
+
+  const camera = new UniversalCamera("viewCamera", new Vector3(1, 0.2, -4), scene);
+  camera.parent = humanoidRoot
 
   // run the main render loop
   engine.runRenderLoop(() => {
+    humanoidRoot.rotate(new Vector3(0, 1, 0), 0.01, Space.WORLD)
     scene.render();
   });
 }
